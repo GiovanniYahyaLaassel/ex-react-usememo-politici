87cb59abcd816ea66c3741160f70f170e501a6ca
Laassel <lassel.giovanni@gmail.com>
2025-06-12 19:55:19 +0200

    feat: implementato campo di ricerca ottimizzato con useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getPoliticians } from "./services/api";
 import CardPolitico from "./components/CardPolitico";
 import './App.css';
@@ -6,6 +6,14 @@ import './App.css';
 
 function App() {
   const [politicians, setPoliticians] = useState([]);
+
+  // Stato per il campo di ricerca
+  const [search , setSearch] = useState("");
+  // funzione per gestire l'input
+  function handleSearchChange(e) {
+    setSearch(e.target.value);
+    // console.log("Valore digitato:", e.target.value);
+  }
 
   useEffect(() => {
     getPoliticians()
@@ -18,13 +26,33 @@ function App() {
       })
   }, []);
 
+  const filteredPoliticians = useMemo(() => {
+    // Ritorno un array filtrato
+    return politicians.filter((p) => {
+      const name = p.name.toLowerCase();
+      const bio = p.biography.toLowerCase();
+      const searchText = search.toLowerCase();
+
+      // controllo se il testo è incluso nel nome o nella biografia 
+      return name.includes(searchText) || bio.includes(searchText);
+    })
+  }, [politicians, search]);
+
   return (
 
      <div>
-        <h1>Lista Politici</h1>
+
+        <input  
+          type="text"
+          placeholder="Cerca per nome o biografia"
+          value={search}
+          onChange={handleSearchChange}
+        />
+
+        <h1>Lista Politici</h1> 
 
         {/* Stampo la lista di poilitici */}
-        {politicians.map((p) => (
+        {filteredPoliticians.map((p) => (
           <CardPolitico
             key={p.id}
             name={p.name}
--- src/services/api.js
+++ src/services/api.js
@@ -4,9 +4,7 @@ import axios from "axios";
 const API_URL = 'http://localhost:3333';
 
 // creo un'istanza con axios con baseURL 
-const api = axios.create({
-    baseURL: API_URL,
-});
+const api = axios.create({ baseURL: API_URL });
 
 // Funzione per ottenere politici
 
